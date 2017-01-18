fe9817bcd4aa470b0c4d788bcff6a252831e792f
Fahad Rehman <Fahad@macs-macbook-air.local>
2017-01-18 11:34:07 +0500

    Initial Commit

---
+++ Searching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Searching
+//
+//  Created by Fahad Rehman on 1/18/17.
+//  Copyright © 2017 Codecture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
